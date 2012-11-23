78697479af44459e0dbab2bbcf3d24da2b37af26
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-23 02:34:47 +0000

    - Adding new app

---
+++ language/en_US/samba_common_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['samba_common_app_name'] = 'Samba Common';
+$lang['samba_common_app_description'] = 'The Samba Common app provides base libraries that can be used in both Samba 3 and Samba 4';
